c3a8e877b1fb6f442bca9f68b72461d46bc19359
David Underwood-Jett <=>
2022-08-22 13:12:14 -0700

    Initial Commit

---
+++ Assets/Shaders/Explorer.cs
@@ -0,0 +1,296 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Explorer : MonoBehaviour
+{
+    public Material mat;
+    public Vector2 pos;
+    public float scale=3.25f;
+    public float angle;
+    public GameObject ControlsPanel;
+
+    public Slider RepeatSlider;
+    public TMP_Text RepeatText;
+
+
+    public Slider RSlider;
+    public TMP_Text RText;
+    public Slider GSlider;
+    public TMP_Text GText;
+    public Slider BSlider;
+    public TMP_Text BText;
+
+    public Slider ColorSlider;
+    public TMP_Text ColorText;
+
+    public Toggle RGBToggle;
+    public Toggle IterateOverTimeToggle;
+
+    public TMP_InputField MaxIterationField;
+    public TMP_InputField IterationFrequencyField;
+
+    public TMP_InputField XModField;
+    public TMP_InputField YModField;
+
+    public bool isIterating = false;
+    public bool isBreathing = false;
+    bool isClimbing = false;
+
+    decimal iterationFrequency = .1M;
+
+    decimal maxIter = 255;
+    public TMP_InputField MinIterationField;
+    decimal minIter = 1;
+    decimal currentIter = 0;
+    float iterationTimer = 0;
+
+    float XMod =1;
+    float YMod =1;
+
+    private void UpdateShader()
+    {
+        float aspect = (float)Screen.width / (float)Screen.height;
+        float scaleX = scale;
+        float scaleY = scale;
+        
+
+        if (aspect > 1f)
+        {
+            scaleY /= aspect;
+        }
+        else
+        {
+            scaleX *= aspect;
+        }
+        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scaleX, scaleY));
+        mat.SetFloat("_Angle", angle);
+        mat.SetFloat("_Repeat", RepeatSlider.value*RepeatSlider.value * RepeatSlider.value);
+        mat.SetFloat("_RValue", RSlider.value * RSlider.value);
+        mat.SetFloat("_GValue", GSlider.value * GSlider.value);
+        mat.SetFloat("_BValue", BSlider.value * BSlider.value);
+        mat.SetFloat("_Color", ColorSlider.value);
+     
+
+
+        if (isIterating)
+        {
+            iterationTimer += Time.deltaTime;
+            if(iterationTimer > (float)iterationFrequency )
+            {
+                if (isBreathing)
+                {//breathing
+                    if (currentIter == maxIter && isClimbing)
+                    {
+                        isClimbing = false;
+                    }
+                    if(currentIter == minIter && !isClimbing)
+                    {
+                        isClimbing = true;
+                        
+                    }
+                    if (isClimbing)
+                    {
+                        mat.SetFloat("_Iter", (float)currentIter);
+                        currentIter += 1;
+                        iterationTimer = 0;
+                    }
+                    else
+                    {
+                        mat.SetFloat("_Iter", (float)currentIter);
+                        currentIter -= 1;
+                        iterationTimer = 0;
+                    }
+                }
+                else
+                {//not breathing
+                    mat.SetFloat("_Iter", (float)currentIter);
+                    currentIter += 1;
+                    iterationTimer = 0;
+                    if (currentIter >= maxIter)
+                    {
+                        currentIter = minIter;
+                    }
+                }
+            }
+            
+
+        }
+
+    }
+
+    private void HandleInputs()
+    {
+        if (Input.GetKey(KeyCode.E)) //Expand
+        {
+            angle -= .01f;
+        }
+        if (Input.GetKey(KeyCode.Q)) //Expand
+        {
+            angle += .01f;
+        }
+
+
+        if (Input.GetKey(KeyCode.Minus) || Input.GetAxis("Mouse ScrollWheel") < 0) //Expand
+        {
+            scale *= 1.010f;
+        }
+        if ((Input.GetKey(KeyCode.Equals) || Input.GetAxis("Mouse ScrollWheel") > 0)&&scale>0) //Expand
+        {
+            scale *= .990f;
+        }
+        if (Input.GetKeyDown(KeyCode.F12)){ 
+
+            HideColorControls();
+        }
+
+        Vector2 dir = new Vector2(.0025f *scale, 0);
+       float s = Mathf.Sin(angle);
+        float c = Mathf.Cos(angle);
+
+        dir = new Vector2(dir.x * c - dir.y * s, dir.x * s + dir.y * c);
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) //Expand
+        {
+            pos -= dir;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)|| Input.GetKey(KeyCode.D)) //Expand
+        {
+            pos += dir;
+        }
+
+        dir = new Vector2(-dir.y, dir.x);
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) //Expand
+        {
+            pos += dir;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) //Expand
+        {
+            pos -= dir;
+        }
+       
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleInputs();
+        UpdateShader();
+        UpdateUIStats();
+    }
+
+    public void HideControls()
+    {
+
+    }
+
+    public void HideColorControls()
+    {
+        ControlsPanel.SetActive(!ControlsPanel.activeSelf);
+    }
+
+    public void UpdateUIStats()
+    {
+        RepeatText.text = "" + RepeatSlider.value * RepeatSlider.value * RepeatSlider.value;
+
+        RText.text = "" + RSlider.value*RSlider.value;
+        GText.text = "" + GSlider.value * GSlider.value;
+        BText.text = "" + BSlider.value * BSlider.value;
+        ColorText.text = "" + ColorSlider.value;
+    }
+    public void ToggleColorType()
+    {
+        if (RGBToggle.isOn)
+        {
+            mat.SetFloat("_IsGradient", 0);
+        }
+        else
+        {
+            mat.SetFloat("_IsGradient", 2);
+        }
+        
+    }
+
+    public void SetMaxIterations()
+    {
+        
+        if (decimal.TryParse(MaxIterationField.text, out maxIter))
+        {
+            if(maxIter > 2000)
+            {
+                maxIter = 2000;
+            }
+            if (maxIter <= minIter && isIterating)
+            {
+                maxIter = minIter + 1;
+            }
+            maxIter += (decimal).0001;
+            if (!isIterating)
+            {
+                mat.SetFloat("_Iter", (float)maxIter);
+            }
+        }
+    }
+
+    public void SetMinIterations()
+    {
+        if (decimal.TryParse(MinIterationField.text, out minIter))
+        {
+            if (minIter <1)
+            {
+                minIter = 1;
+            }
+            if(minIter >= maxIter)
+            {
+                minIter = maxIter - 1;
+            }
+            minIter += (decimal).0001;
+           
+        }
+    }
+    public void ToggleIterationOverTime()
+    {
+        if (IterateOverTimeToggle.isOn)
+        {
+            isIterating = true;
+            if (minIter >= maxIter)
+            {
+                minIter = 1;
+                maxIter = 100;
+            }
+            iterationTimer = 0;
+            currentIter = minIter;
+            mat.SetFloat("_Iter", (float)currentIter);
+           
+        }
+        else
+        {
+            isIterating = false;
+            iterationTimer = 0;
+        }
+
+
+    }
+    public void SetIterationFrequency()
+    {
+        if (decimal.TryParse(IterationFrequencyField.text, out iterationFrequency))
+        {
+            if(iterationFrequency < (decimal).000001)
+            {
+                iterationFrequency = (decimal).000001;
+            }
+        }
+    }
+
+    public void SetModulationFloats()
+    {
+        float.TryParse(XModField.text, out XMod);
+        float.TryParse(YModField.text, out YMod);
+        mat.SetFloat("_XMod", XMod);
+        mat.SetFloat("_YMod", YMod);
+    }
+    public void ToggleBreathing()
+    {
+        isBreathing = !isBreathing;
+    }
+}
---
+++ Assets/Shaders/Mandelbrot.shader
@@ -0,0 +1,197 @@
+﻿Shader "Foundry/Mandelbrot"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Area("Area", vector) = (0,0,4,4) 
+        _Angle ("Angle", range(-3.1415, 3.1415)) = 0
+        _Iter ("Iter", float) = 1000
+        _Color ("Color", range(0,1)) =.5
+        _Repeat ("Repeat", float) = 1
+        _IsGradient ("IsGradient", float) = 0
+        _RValue ("RValue", float) = 70
+        _GValue ("GValue", float) = .5
+        _BValue ("BValue", float) = .5
+        _JuliaNValue("NValue", float) = 3
+        _FractalType ("FractalType", int) = 0
+        _Tolerance ("Tolerance", float) = 0.000001
+        _XMod ("XMod", float) = 1
+        _YMod ("YMod", float) = 1
+
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+// Upgrade NOTE: excluded shader from DX11, OpenGL ES 2.0 because it uses unsized arrays
+//#pragma exclude_renderers d3d11 gles
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+            float4 _Area;
+            float _Angle;
+            sampler2D _MainTex;
+            float _Iter;
+            float _Color;
+            float _Repeat;
+            float _IsGradient;
+            float _RValue;
+            float _GValue;
+            float _BValue;
+            int _FractalType;
+            float2 difference;//newton
+            float _Tolerance;//newton
+            
+            float _XMod; //XMod and Ymod allow for slight variations within the fractals overall design
+
+            float _YMod;
+
+            float4 col;
+           
+            float2 rot(float2 p, float2 pivot, float a){
+            float s = sin(a);
+            float c = cos(a);
+            p -= pivot;
+            p = float2(p.x*c - p.y*s ,p.x*s+p.y*c);
+            p+= pivot;
+
+            return p;
+            }
+
+            float2 CDiv (float2 numerator, float2 denominator)
+{
+   return  float2(((numerator.x * denominator.x)+(numerator.y*denominator.y)/(denominator.x*denominator.x+denominator.y*denominator.y)), ((numerator.y*denominator.x-numerator.x*denominator.y)/(denominator.x*denominator.x+denominator.y*denominator.y)));
+
+
+}
+
+
+float2 CMul (float2 a, float2 b)
+{
+   return float2(a.x*b.x-a.y*b.y,a.x*b.y+b.x*a.y);
+}
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+              float2  c = _Area.xy + (i.uv-.5)*_Area.zw;
+              
+              c = rot(c,_Area.xy,_Angle);
+            
+              float2 z;
+              float zy;
+              float zx;
+              float xTemp;
+
+             if(_FractalType <=1){
+              
+              float n =3;
+             
+              for(int iter; iter< _Iter; iter++){
+             
+              if(_FractalType == 0){
+              z = float2(z.x*z.x-z.y*z.y*_XMod,2*z.x*z.y*_YMod) + c; //MANDELBROT
+              }
+             /*xTemp = pow(z.x*z.x+z.y*z.y, n/2) * cos(n *atan2(z.y,z.x)) + c.x;
+             zy = pow(z.x*z.x+z.y*z.y, n/2) * sin(n *atan2(z.y,z.x)) + c.y;
+             zx = xTemp;
+             z = float2(zx,zy);*/
+
+             else if(_FractalType == 1){
+             /*BURNING SHIP*/
+             xTemp = z.x*z.x-z.y*z.y+c.x;
+             z.y = abs(2*z.x*z.y)*_YMod + c.y ;
+             z.x = abs(xTemp)*_XMod;
+             }
+            
+           
+              if(length(z) >25){
+              break;
+              }
+              }
+             if(iter > _Iter){
+              return 0;
+              }
+
+              
+              float m = pow((iter/_Iter),.3);
+              if(_IsGradient < 1){
+              col = sin(float4(_RValue,_GValue,_BValue,1)*m*_Repeat)*.5 + .5; //procedural colors
+             }
+             else{
+              col = tex2D(_MainTex, float2(m*_Repeat, _Color));
+             }
+              return col;
+            }//end of [-infity,1] fractals 
+            else if(_FractalType == 2){
+
+            float2 v = 0;
+            float m = 0;
+            float r = 100;
+
+            for(int n = 0; n<_Iter; n++){
+            v = float2(v.x*v.x-v.y*v.y*v.y,v.x*v.y*2)+c;
+
+            if(dot(v,v)<(r*r-1)){
+            m++;
+            }
+            v = clamp(v,-r,r);
+            
+            
+            
+            }//end of for loop
+
+            if(m == _Iter){
+            return float4(0,0,0,1);
+            }else{
+            return float4(sin(m/4) ,sin(m/5),sin(m/7),1)/4 + .75f;
+            }
+
+
+            return(0,0,0,1);
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            else{
+            return float4(0,0,0,1);
+                   
+            
+           
+            
+           }
+           // return iter;
+            }
+            ENDCG
+        }
+    }
+}
